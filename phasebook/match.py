95df3e0860d5f184492021aa2b446544bccf5fa6
Henry William Yukit <hgyukit@up.edu.ph>
2022-10-10 15:47:03 +0800

    [ENHANCEMENT]: Enhanced matching algorithm. Used merge sort algorithm so that we can use binary search algorithm.

--- phasebook/match.py
+++ phasebook/match.py
@@ -6,6 +6,71 @@ from .data.match_data import MATCHES
 
 bp = Blueprint("match", __name__, url_prefix="/match")
 
+#reference for merge sort algorithm: https://www.geeksforgeeks.org/python-program-for-merge-sort/
+def merge(arr, l, m, r):
+    n1 = m - l + 1
+    n2 = r - m
+ 
+    L = [0] * (n1)
+    R = [0] * (n2)
+ 
+    for i in range(0, n1):
+        L[i] = arr[l + i]
+ 
+    for j in range(0, n2):
+        R[j] = arr[m + 1 + j]
+ 
+    i = 0     
+    j = 0     
+    k = l
+ 
+    while i < n1 and j < n2:
+        if L[i] <= R[j]:
+            arr[k] = L[i]
+            i += 1
+        else:
+            arr[k] = R[j]
+            j += 1
+        k += 1
+ 
+    while i < n1:
+        arr[k] = L[i]
+        i += 1
+        k += 1
+ 
+    while j < n2:
+        arr[k] = R[j]
+        j += 1
+        k += 1
+ 
+def mergeSort(arr, l, r):
+    if l < r:
+        m = l+(r-l)//2
+ 
+        mergeSort(arr, l, m)
+        mergeSort(arr, m+1, r)
+        merge(arr, l, m, r)
+
+#reference for binary search algorithm: https://www.geeksforgeeks.org/python-program-for-binary-search/
+def binary_search(arr, x):
+    low = 0
+    high = len(arr) - 1
+    mid = 0
+ 
+    while low <= high:
+ 
+        mid = (high + low) // 2
+ 
+        if arr[mid] < x:
+            low = mid + 1
+ 
+        elif arr[mid] > x:
+            high = mid - 1
+ 
+        else:
+            return mid
+ 
+    return -1
 
 @bp.route("<int:match_id>")
 def match(match_id):
@@ -20,8 +85,10 @@ def match(match_id):
 
 
 def is_match(fave_numbers_1, fave_numbers_2):
-    for number in fave_numbers_2:
-        if number not in fave_numbers_1:
+    mergeSort(fave_numbers_1, 0, len(fave_numbers_1) - 1)
+    mergeSort(fave_numbers_2, 0, len(fave_numbers_2) - 1)
+    for number in fave_numbers_1:
+        if binary_search(fave_numbers_2, number) == -1:
             return False
 
     return True
